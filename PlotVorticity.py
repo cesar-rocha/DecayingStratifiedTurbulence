5857d7e2146184c663326e661af8fd2dee7ff310
cesar-rocha <rocha.cesarb@protonmail.com>
2021-03-08 20:43:10 -0500

    Add scripts

---
+++ PlotVorticity.py
@@ -0,0 +1,38 @@
+import h5py
+import numpy as np 
+from numpy import pi
+import matplotlib.pyplot as plt
+import cmocean 
+import proplot 
+
+plt.close('all')
+
+# load simulation data
+setup = h5py.File('output/setup.h5','r')
+snapshot = h5py.File('output/snapshots/000000000000030.h5','r')
+
+# plot vorticity 
+kw = dict(cmap=cmocean.cm.balance,
+          vmin=-10,vmax=10)
+          
+ticks = [0,pi/2,pi,3*pi/2,2*pi]
+ticklabels = [r'0',r'$\pi/2$',r'$\pi$',
+              r'$3\pi/2$',r'$2\pi$']
+
+fig = plt.figure(figsize=(5,5))
+
+ax = fig.add_subplot(111,aspect=1)
+
+plt.pcolormesh(setup['grid/x'],
+               setup['grid/z'],
+               snapshot['q'],
+               **kw
+            )
+
+plt.xticks(ticks,ticklabels)
+plt.yticks(ticks,ticklabels)
+plt.xlabel('x')
+plt.ylabel('z')
+
+plt.savefig('figure1a.png',bbox_inches='tight')
+plt.savefig('figure1a.eps',bbox_inches='tight')
---
+++ RunTheModel.py
@@ -0,0 +1,59 @@
+import numpy as np
+from TwoDimBoussinesq import spectral_model as model
+
+#
+# Initialize TwoDimBoussinesq model object
+#
+Fr  = 0.002
+Reb = 20
+
+m = model.Boussinesq2d(
+                Lx=2.*np.pi,
+                nx=1024, 
+                tmax = 30, 
+                dt = 0.001,
+                ntd=8,Fr=Fr,
+                tsave=100,
+                twrite=200,
+                nu=(Fr**2)/Reb,
+                sig=1.e5,
+                kf=25,
+                ext_forc=True,
+                use_fftw=False,
+                use_filter=True,
+                )
+
+#
+# Set up initial conditions
+#
+
+# a von-Karman-like spectrum with random phase
+fk = m.kappa != 0
+ckappa = np.zeros_like(m.kappa2)
+ckappa[fk] = np.sqrt(m.kappa2[fk]*(1. + 
+                    (m.kappa2[fk]/36.)**2) )**-1
+
+nhx,nhy = m.kappa2.shape
+Pi_hat = (np.random.randn(nhx,nhy) + \
+          1j*np.random.randn(nhx,nhy) )*ckappa
+
+Pi = m.ifft2(Pi_hat)
+Pi = Pi - Pi.mean()
+Pi_hat = m.fft2(Pi)
+KEaux = m.spec_var(m.kappa*Pi_hat)
+pih = (Pi_hat/np.sqrt(KEaux))
+qih = -m.kappa2*pih
+qi = m.ifft2(qih)
+
+# unforced solution (fq = 0)
+m.set_forcing(m.z*0)
+
+# vorticity and buoyancy 
+m.set_q(qi)
+m.set_b(qi*0)
+
+#
+# Integrate the model
+#
+m.run()
+
